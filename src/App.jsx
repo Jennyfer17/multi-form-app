3742e48a512511baedc6fccb1d4707484fef3251
Jennyfer17 <tholejennyfer@gmail.com>
2024-12-01 02:39:07 +0200

    initializing project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import PersonalInfo from './components/PersonalInfo'
+import { Container } from 'postcss'
+function App() {
+  return (
+    <>
+      <PersonalInfo/>
+    </>
+  )
+}
+
+export default App
